cd3c322c16eb8bb5ff706e6e9b365a65fe995dcb
fauzanmi18 <iqbal.darussalam17@gmail.com>
2024-02-23 11:28:44 +0700

    ui: re-design dashboard card for mobile on customer page

--- src/components/dashboard/pendidikan/pendidikan.vue
+++ src/components/dashboard/pendidikan/pendidikan.vue
@@ -1,19 +1,15 @@
 <template>
-    <div v-if="userResultDetect" class="bg-white rounded-lg shadow-sm p-4 h-full">
-        <img class="w-24 drop-shadow-2xl shadow-dark -mt-3 mb-2 animate-wiggle" src="../../../assets/img/pendidikan.png" alt="">
-        <!-- <p class="font-myFont font-semibold text-start mx-12 lg:mx-4 text-gray-500 text-sm mb-2">List pendidikan :</p> -->
-        <div class="mt-2 mb-2">
-            <div class="w-full flex flex-col">
-                <h1 class="font-myFont text-dark text-lg mx-4 font-semibold mb-1">Pendidikan yang optimal untukmu</h1>
-                <div class="mx-12 lg:mx-4 flex flex-wrap justify-start gap-2">
-                    <span
-                    v-for="(item, index) in listPendidikan"
-                    :key="index"
-                    class="px-4 py-2 text-xs text-biru border rounded-full border-biru font-myFont"
-                    >
-                    {{ item }}
-                    </span>
-                </div>
+    <div class="mt-2 mb-2">
+        <div class="w-full flex flex-col">
+            <h1 class="font-myFont text-dark text-lg font-semibold mb-2">Pendidikan yang optimal untukmu</h1>
+            <div class="lg:mx-4 flex flex-wrap justify-start gap-2">
+                <span
+                v-for="(item, index) in listPendidikan"
+                :key="index"
+                class="px-4 py-2 text-xs text-biru border rounded-full border-biru font-myFont"
+                >
+                {{ item }}
+                </span>
             </div>
         </div>
     </div>
