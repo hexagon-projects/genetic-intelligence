47dac407a36cce8e825ab552da2aab3b7623b5d9
fauzanmi18 <iqbal.darussalam17@gmail.com>
2024-03-03 07:14:15 +0700

    ui: adjust new login for responsive device

--- src/components/auth/login/newLogin.vue
+++ src/components/auth/login/newLogin.vue
@@ -1,13 +1,13 @@
 <template>
-    <div class="hidden lg:block">
-      <div id="img-login" class="w-full flex items-center justify-between min-h-screen px-12 py-8 xl:py-18">
-        <div class="text-white flex flex-col gap-1 ml-10">
+    <div class="block">
+      <div id="img-login" class="w-full flex flex-col lg:flex-row items-center lg:justify-between min-h-screen px-4 lg:px-12 py-8 xl:py-18">
+        <div class="hidden text-white lg:flex flex-col gap-1 ml-10">
             <h1 class="text-lg md:text-4xl lg:text-3xl font-myFont font-semibold">#TemukanJatidirimu</h1>
             <h1 class="text-2xl md:text-4xl lg:text-5xl font-myFont font-semibold lg:mb-4">Bersama Jatidiri.app</h1>
             <!-- <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur illum non error eaque adipisci inventore!</p> -->
         </div>
 
-        <div class="bg-white bg-opacity-60 backdrop-blur-xl h-[579px] w-1/3 rounded-3xl">
+        <div class="bg-white bg-opacity-60 backdrop-blur-xl h-[579px] md:h-[640px] lg:h-[579px] w-full lg:w-1/3 rounded-3xl">
             <img src="../../../assets/img/logo-new.png" class="w-1/3 mx-auto mb-6 mt-8">
             <h1 class="text-center text-2xl font-myFont font-semibold">Masuk</h1>
             <p class="text-center font-myFont text-gray-500 mb-4">Silahkan masuk dengan akun anda.</p>
@@ -22,7 +22,7 @@
                   <a v-if="passwordValidation && password.length < 1" class="text-xs text-red-500">Password tidak boleh kosong.</a>
                 </div>
                 <div class="mx-auto w-full flex flex-col items-center my-3 gap-4">
-                  <button v-if="isLoading" disabled="" type="button" class="font-myFont font-medium bg-biru text-white py-2 px-4 rounded-md hover:bg-blue-600 w-full lg:w-3/4 inline-flex justify-center items-center">
+                  <button v-if="isLoading" disabled="" type="button" class="font-myFont font-medium bg-biru text-white py-2 px-4 rounded-md hover:bg-blue-600 w-3/4 inline-flex justify-center items-center">
                       <svg aria-hidden="true" role="status" class="inline mr-3 w-4 h-4 text-white animate-spin" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="#E5E7EB"></path>
                         <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentColor"></path>
