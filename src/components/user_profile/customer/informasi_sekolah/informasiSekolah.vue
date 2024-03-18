18a010860b0ea2dd6d162c43407686614a4c9d46
fauzanmi18 <iqbal.darussalam17@gmail.com>
2024-03-18 14:52:36 +0700

    ui: adjust user profile customer pelajar

---
+++ src/components/user_profile/customer/informasi_sekolah/informasiSekolah.vue
@@ -0,0 +1,60 @@
+<template>
+    <div class="bg-white rounded-lg shadow-sm p-4">
+        <h2 class="font-myFont text-dark text-2xl mb-4">Informasi Sekolah</h2>
+        <div class="flex flex-col justify-center items-center">
+            <div class="flex justify-center items-center w-full gap-2">
+                <div class="w-full mb-4">
+                    <label for="jenjangPendidikan" class="block text-sm font-myFont font-medium text-dark">Jenjang Pendidikan:</label>
+                    <input v-model="jenjangPendidikan" type="text" name="jenjangPendidikan" class="mt-1 p-2 border rounded-md w-full focus:outline-none focus:ring-biru focus:ring-2 focus:border-biru" placeholder="Password anda saat ini" />
+                </div>
+                <div class="w-full mb-4">
+                    <label for="namaPendidikan" class="block text-sm font-myFont font-medium text-dark">Nama Sekolah:</label>
+                    <input v-model="namaPendidikan" type="text" name="namaPendidikan" class="mt-1 p-2 border rounded-md w-full focus:outline-none focus:ring-biru focus:ring-2 focus:border-biru" placeholder="Password baru anda" />
+                </div>
+            </div>
+
+            <div class="flex justify-center items-center w-full gap-2">
+                <div class="w-full mb-4">
+                    <label for="currPass" class="block text-sm font-myFont font-medium text-dark">Jurusan:</label>
+                    <input v-model="currPass" type="text" name="curPass" class="mt-1 p-2 border rounded-md w-full focus:outline-none focus:ring-biru focus:ring-2 focus:border-biru" placeholder="Jurusan" />
+                </div>
+                <div class="w-full mb-4">
+                    <label for="newPass" class="block text-sm font-myFont font-medium text-dark">Kelas:</label>
+                    <input v-model="newPass" type="text" name="newPass" class="mt-1 p-2 border rounded-md w-full focus:outline-none focus:ring-biru focus:ring-2 focus:border-biru" placeholder="Kelas" />
+                </div>
+            </div>
+
+            <button @click="changePassword" class="px-2 py-2 w-1/2 lg:w-1/4 self-center text-center rounded-lg bg-biru font-myFont font-medium text-light hover:opacity-75 hover:shadow-lg">
+                Ubah Data
+            </button>
+        </div>
+    </div>
+</template>
+
+<script>
+import { ref } from 'vue'
+import initAPI from '../../../../api/api'
+import Swal from 'sweetalert2';
+import 'sweetalert2/dist/sweetalert2.css';
+
+export default {
+    name: 'InformasiSekolah',
+    props: ['dataCustomer'],
+    setup(props){
+        console.log('data props is', props)
+
+        const jenjangPendidikan = ref(props.dataCustomer.institutions.type)
+        const namaPendidikan = ref(props.dataCustomer.institutions.name)
+        const kelas = ref(props.dataCustomer.institutions.type)
+        const jurusan = ref(props.dataCustomer.institutions.type)
+
+        return {
+            jenjangPendidikan,
+            namaPendidikan,
+            kelas,
+            jurusan,
+            props
+        }
+    }
+}
+</script>
