b7ce5cb3036f06464a3641d531073514f91ab094
fauzanmi18 <iqbal.darussalam17@gmail.com>
2024-03-20 13:04:12 +0700

    ui: adjust bg login

--- src/components/auth/login/newLogin.vue
+++ src/components/auth/login/newLogin.vue
@@ -170,7 +170,7 @@ export default {
     
 <style scoped>
     #img-login {
-      min-height:100%;
+      min-height:100vh;
       background: linear-gradient(rgba(0, 0, 0, 0.1), rgba(0, 0, 0, 0.2)), url("../../../assets/img/bg-login2.jpg");
       /* background:linear-gradient(0deg, rgba(255, 0, 150, 0.3), rgba(38, 173, 197, 0.3)), url("../../../assets/img/bg-login2.jpg"); */
       background-repeat: no-repeat;
