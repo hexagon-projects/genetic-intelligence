f3f1d281ff0022d066d5ac97750f8c8b58b07277
fauzanmi18 <iqbal.darussalam17@gmail.com>
2024-04-18 00:48:32 +0700

    ui ready for forgot password

--- src/components/auth/forgot/forgot.vue
+++ src/components/auth/forgot/forgot.vue
@@ -1,21 +1,27 @@
 <template>
     <div id="img-login" class="w-full flex flex-row items-center justify-center min-h-screen px-4">
         <div class="flex justify-center pt-22">
-            <div class="bg-white mx-4 lg:mx-auto px-10 py-4 drop-shadow-2xl rounded-lg">
+            <div class="w-[640px] bg-white mx-4 lg:mx-auto px-10 py-4 drop-shadow-2xl rounded-lg">
                 <div class="flex flex-col justify-center items-center mb-6">
                     <h2 class="text-black text-lg lg:text-2xl font-myFont font-bold mb-2 mt-5">Lupa Password</h2>
                     <a class="font-myFont text-gray-500 text-xs lg:text-sm mb-4">
-                        Kami akan mengirim email apabila email yang anda cantumkan sudah terdaftar
+                        {{ !isEmailSent ? 'Kami akan mengirim email apabila email yang anda cantumkan sudah terdaftar' 
+                        : 'Masukkan 4 digit kode verifikasi yang dikirimkan ke email Anda.'
+                        }}
                     </a>
 
-                    <div class="mt-4 w-full lg:w-1/2 mb-4">
+                    <div v-if="!isEmailSent" class="mt-4 w-full lg:w-1/2 mb-4">
                         <input v-model="email" @input="validation" type="email" name="email"
-                        :class="{'border-danger': validasiEmail !== null, 'border-gray-300': validasiEmail == null}"
+                        :class="{'border-danger outline-none': validasiEmail !== null, 'border-gray-300': validasiEmail == null}"
                         class="placeholder:text-base font-myFont rounded-md border w-full py-2 px-3" placeholder="Masukan Email Anda" autocomplete="off">
-                        <p v-if="validasiEmail !== null" class="font-myFont text-red-500 text-xs italic">{{ validasiEmail }}.</p>
+                        <p v-if="validasiEmail !== null" class="mt-1 font-myFont text-red-500 text-xs italic">{{ validasiEmail }}.</p>
+                    </div>
+
+                    <div v-if="isEmailSent" class="mb-4">
+                        <otpForm/>
                     </div>
                     
-                    <button @click="kirimEmail" :disabled="buttonDisabled"
+                    <button v-if="!isEmailSent" @click="kirimEmail" :disabled="buttonDisabled"
                     :class="{'bg-gray-600 opacity-80 cursor-not-allowed': buttonDisabled}" 
                     class="w-full lg:w-1/2 mb-4 font-myFont bg-biru text-white py-2 px-4 rounded-lg hover:opacity-75 hover:shadow-lg">Kirim Email</button>
 
@@ -28,10 +34,15 @@
 
 <script>
 import { ref, computed } from 'vue'
+import otpForm from './otp.vue'
+import DOMPurify from 'dompurify'
 
 export default {
     name: 'ForgotPage',
+    components: { otpForm },
     setup(){
+        
+        const isEmailSent = ref(false)
         const email = ref('')
         const validasiEmail = ref(null)
 
@@ -50,10 +61,12 @@ export default {
         }
 
         const kirimEmail = () => {
-            console.log('oke dikirim', email.value)
+            console.log('oke dikirim', DOMPurify.sanitize(email.value))
+            isEmailSent.value = !isEmailSent.value
         }
 
         return {
+            isEmailSent,
             email,
             validasiEmail,
             buttonDisabled,
