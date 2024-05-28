2cf9fcb1743941d448cdde5efdfd2a78f4ccb353
fauzanmi18 <iqbal.darussalam17@gmail.com>
2024-05-28 14:29:29 +0700

    validation router pembayaran

--- src/router/router.js
+++ src/router/router.js
@@ -118,6 +118,9 @@ const routes = [
             if (!token || !isAuth) {
                 // Jika token tidak ada, arahkan pengguna ke halaman login
                 next({ name: 'views.login' });
+            } else if(isAuth && isAuth.is_payment_gim == 'Tidak') {
+                router.push('/pembayaran/test-assessment')
+                next() 
             } else {
                 const decodedToken = jwtDecode(token);
                 console.log(`di deocde cek`, decodedToken);
@@ -165,6 +168,9 @@ const routes = [
             const isAuth = JSON.parse(localStorage.getItem('userData'))
             if (!token || !isAuth) {
                 next({ name: 'views.login' });
+            } else if(isAuth && isAuth.is_payment_gim == 'Tidak') {
+                router.push('/pembayaran/test-iq')
+                next()
             } else {
                 const decodedToken = jwtDecode(token);
                 console.log(`di deocde cek`, decodedToken);
