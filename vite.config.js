638c46890b1866bb93c75a56e6dcb955d7c3c81c
fauzanmi18 <iqbal.darussalam17@gmail.com>
2024-08-29 02:15:33 +0700

    adjust styling CPM

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,16 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import 'dotenv/config'
+import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
   optimizeDeps: {
     include: ['chart.js/auto']
   }
