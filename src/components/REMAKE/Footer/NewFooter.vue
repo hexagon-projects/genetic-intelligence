1afd8f10bdfd67076e521f7ecb2bde37db7d0208
fauzanmi18 <iqbal.darussalam17@gmail.com>
2024-09-01 09:56:47 +0700

    finish slicing new footer

---
+++ src/components/REMAKE/Footer/NewFooter.vue
@@ -0,0 +1,27 @@
+<template>
+    <footer id="footer" class="bg-white pt-4 pb-4">
+         <div class="container mx-auto">
+             <div class="flex justify-between items-center container mx-auto w-full">
+                <img class="w-[121px] h-[48px]" src="@/assets/img/logo-new.png">
+
+                <p class="font-medium font-roboto text-xs lg:text-sm text-[#6c748f] text-center">
+                   Copyright © {{ currentYear }} Jatidiri. All Rights Reserved.
+                </p>
+                
+                <div class="group cursor-pointer flex justify-center items-center gap-2">
+                    <a class="p-1 border border-[#3030f8] rounded-[50%] text-[#3030f8] group-hover:bg-[#3030f8] group-hover:text-white">
+                        <PhInstagramLogo :size="18"/>
+                    </a>
+
+                    <span class="font-medium text-[#6c748f] text-sm font-roboto">Jatidiri.app</span>
+                </div>
+             </div>
+         </div>
+     </footer>
+</template>
+
+<script setup>
+import { PhInstagramLogo } from '@phosphor-icons/vue';
+</script>
+
+<!-- src="@/assets/img/logo-new.png" -->
