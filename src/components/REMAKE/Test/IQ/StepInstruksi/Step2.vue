657bc5422deb22b6639980322c5d000cee6dae06
fauzanmi18 <iqbal.darussalam17@gmail.com>
2024-09-25 01:06:35 +0700

    adjust bold waktu instruksi

--- src/components/REMAKE/Test/IQ/StepInstruksi/Step2.vue
+++ src/components/REMAKE/Test/IQ/StepInstruksi/Step2.vue
@@ -3,7 +3,7 @@
         <img src="@/assets/icons/test_iq/step2_instruksi.svg" alt="icon"/>
 
         <span class="text-center text-[#667084] text-sm md:text-base font-normal font-['Roboto'] leading-normal">
-            Anda memiliki <span class="font-semibold">waktu 12</span> menit untuk memberi jawaban yang benar sebanyak mungkin. Kerjakan dengan teliti, namun jangan menghabiskan waktu lama pada setiap pertanyaan atau lewati pertanyaan itu.
+            Anda memiliki waktu <span class="font-semibold">12 menit</span> untuk memberi jawaban yang benar sebanyak mungkin. Kerjakan dengan teliti, namun jangan menghabiskan waktu lama pada setiap pertanyaan atau lewati pertanyaan itu.
         </span>
     </div>
 </template>
