ba9650237ce0ca2a86ffe370283c3cbfea5364c0
MSyahidAlFajri <masyahida963@gmail.com>
2024-10-06 21:21:41 +0700

    UI Layout RMIB

---
+++ src/views/REMAKE/Customer/Test/RMIB/SoalEssayRmib.vue
@@ -0,0 +1,45 @@
+<template>
+<div class="w-full h-auto flex justify-center pt-2 pb-28">
+    <div class="mx-[30px] md:mx-[60px] w-[872px] h-auto p-9 bg-white rounded-3xl shadow flex-col justify-center items-center gap-6 inline-flex">
+        <div class="w-full flex flex-col lg:flex-row md:justify-between items-center">
+            <span class="text-black text-lg md:text-xl lg:text-2xl font-medium font-['Roboto'] leading-loose">Jawablah Pertanyaan Dibawah Ini</span>
+        </div>
+
+        <!-- Soal --> 
+        <div class="self-start w-full">
+            <div class="flex flex-col mb-[24px]">
+                <div class="text-black text-base font-normal font-['Roboto'] leading-normal">
+                    <span class="w-full text-black text-sm md:text-base font-normal font-['Roboto'] leading-normal">
+                        Tuliskan di bawah ini tiga (3) macam pekerjaan yang paling ingin Kamu lakukan atau paling Kamu sukai (tidak harus pekerjaan yang tercantum dalam daftar yang sebelumnya ada)
+                    </span>
+                </div>
+            </div>
+
+            <div class="w-full flex flex-col gap-4">
+                <div class="flex items-center">
+                    <span class="text-black text-base font-normal">1.</span>
+                    <input type="text" class="w-full p-2 border rounded-md border-[#3030f8] text-[#667084] outline-none ml-2" placeholder="Masukkan pekerjaan yang paling Anda sukai" />
+                </div>
+                <div class="flex items-center">
+                    <span class="text-black text-base font-normal">2.</span>
+                    <input type="text" class="w-full p-2 border rounded-md border-[#3030f8] text-[#667084] outline-none ml-2" placeholder="Masukkan pekerjaan yang paling Anda sukai" />
+                </div>
+                <div class="flex items-center">
+                    <span class="text-black text-base font-normal">3.</span>
+                    <input type="text" class="w-full p-2 border rounded-md border-[#3030f8] text-[#667084] outline-none ml-2" placeholder="Masukkan pekerjaan yang paling Anda sukai" />
+                </div>
+            </div>
+        </div>
+
+        <div class="flex flex-col items-center gap-[24px]">
+            <button class="hover:shadow-2xl hover:translate-x-1 transition-all h-11 pl-6 pr-4 py-1.5 bg-[#3030f8] rounded-full justify-center items-center gap-3 inline-flex">
+                <div class="text-white text-base font-normal font-['Roboto'] leading-normal">Kirim Jawaban</div>
+                <div class="w-6 h-6 relative">
+                    <img src="@/assets/icons/test_gim/chevron_right.svg" alt="icon"/>
+                </div>
+            </button>
+        </div>
+    </div>
+</div>
+
+</template>
---
+++ src/views/REMAKE/Customer/Test/RMIB/StepInstruksi/Step3.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="flex flex-col items-center gap-[56px]">
+        <span class="text-center text-[#667084] text-base font-normal font-['Roboto'] leading-normal">
+           Bekerjalah secepatnya, dan tuliskan nomor-nomor sesuai dengan kesan dan keinginan Kamu yang pertama muncul.
+        </span>
+
+        <button @click="showSoal" class="hover:-translate-y-1 transition-all h-11 px-6 py-1.5 bg-[#3030f8] rounded-full justify-center items-center gap-3 inline-flex">
+            <div class="text-white text-sm md:text-base font-normal font-['Roboto'] leading-normal">Mulai Tes</div>
+        </button>
+    </div>
+</template>
+
+<script setup>
+import { useStore } from 'vuex';
+
+const store = useStore()
+
+const showSoal = () => {
+    store.commit('setContohSoalIQ', true)
+}
+</script>
