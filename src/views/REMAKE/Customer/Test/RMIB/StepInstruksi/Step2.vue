55a785a1d5a192cffbca97e4d54d82f9e68f9c07
Daffa Audya P <daffaaudyapramana03@gmail.com>
2024-10-07 11:07:14 +0700

    Update UI RMIB

--- src/views/REMAKE/Customer/Test/RMIB/StepInstruksi/Step2.vue
+++ src/views/REMAKE/Customer/Test/RMIB/StepInstruksi/Step2.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="flex flex-col items-center gap-[56px]">
-        <img src="" alt="icon"/>
+        <img src="../img/intruksi2.svg" alt="icon"/>
 
         <span class="text-justify text-[#667084] text-sm md:text-base font-normal font-['Roboto'] leading-normal">
             Pilih pekerjaan dari setiap kelompok yang Kamu sukai, beri nomor 1 untuk yang paling disukai, diikuti 2, 3, dan seterusnya, serta nomor 12 untuk yang paling tidak disukai.
