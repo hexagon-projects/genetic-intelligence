6accaa538a659cc78fc874024861fa7f3a74e804
isnaenti <isnaenti16@gmail.com>
2024-10-07 22:45:44 +0700

    edit UI hasil

---
+++ src/views/REMAKE/Customer/Test/Gadget/HasilGadget.vue
@@ -0,0 +1,28 @@
+<template>
+    <div v-if="loading" class="preloader-overlay">
+        <span class="flex justify-center animate-[spin_2s_linear_infinite] border-8 border-[#f1f2f3] border-l-biru border-r-biru rounded-full w-14 h-14 m-auto"></span>
+    </div>
+
+    <Layout v-if="!loading">
+        <div class="mx-0 lg:mx-[40px] mb-3 h-5 p-7 justify-center items-center gap-2 inline-flex">
+            <div class="text-[#3030f8] text-sm font-normal font-roboto leading-tight">Beranda</div>
+            <div class="w-4 h-4 relative opacity-75">
+                <img src="@/assets/img/chevron_forward.svg">
+            </div>
+            <div class="text-[#3030f8] text-sm font-normal font-roboto leading-tight">Hasil Tes</div>
+            <div class="w-4 h-4 relative opacity-75">
+                <img src="@/assets/img/chevron_forward.svg">
+            </div>
+            <div class="opacity-75 text-black text-sm font-normal font-roboto leading-tight">Hasil Test IAA</div>
+        </div>
+        <div class="flex items-center bg-white">
+            <img src="@/assets/img/logo-jatidiri-hasi-cpm.png">
+            <span class="text-center text-[#0B0B79] font-normal font-['Sora']">Hasil Tes Internet Addiction Assessment</span>
+        </div>
+    </Layout>
+</template>
+<script>
+import Layout from '@/Layout/Customer/Layout.vue';
+import Reservasi from '@/components/REMAKE/ReservasiFooter/Reservasi.vue';
+import BelumTest from '@/components/REMAKE/HasilTest/BelumTest/BelumTest.vue';
+</script>
