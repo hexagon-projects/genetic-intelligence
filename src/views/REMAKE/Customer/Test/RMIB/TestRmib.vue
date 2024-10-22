a87e0d9f3f0ecc5c0f4707e9e9f7a35ed181cf9b
“MSyahidAlFajri” <masyahida4@gmail.com>
2024-10-22 11:57:42 +0700

    update logic untuk soal rmib, fix error pengambilan custoner rmib id, dan update untuk selesai test

--- src/views/REMAKE/Customer/Test/RMIB/TestRmib.vue
+++ src/views/REMAKE/Customer/Test/RMIB/TestRmib.vue
@@ -14,7 +14,7 @@
         <modalCekProfile/>
     </div>
 
-    <Layout>
+    <Layout v-if="!loading">
         <div class="mx-0 lg:mx-[40px] mb-3 h-5 p-7 justify-center items-center gap-2 inline-flex">
             <div class="text-[#3030f8] text-sm font-normal font-roboto leading-tight">Beranda</div>
             <div class="w-4 h-4 relative opacity-75">
@@ -27,10 +27,11 @@
             <div class="opacity-75 text-black text-sm font-normal font-roboto leading-tight">Test RMIB</div>
         </div>
 
-        <!-- <SelesaiTest v-if="isTested" routeUrl="user.views.hasil_iq" message="Test IQ Selesai!"
-        :subMessage="subMessage"/> -->
+        <SelesaiTest v-if="isTested" message="Test RMIB Selesai!"
+        :subMessage="subMessage"/>
+        <!-- routeUrl="user.views.hasil_RMIB" -->
 
-        <section class="pb-[34px] w-full bg-white">
+        <section v-if="!isTested" class="pb-[34px] w-full bg-white">
             <transition name="fade" mode="out-in">
                 <div v-if="isKebijakanPrivasi" class="fixed z-[999] inset-0 bg-gray-900 bg-opacity-60 overflow-y-auto h-full w-full px-4 modal"
                 >
@@ -46,10 +47,9 @@
                 <SoalTest v-if="!showEssay && customerGen" :customerGen="customerGen" :customerId="customerId" @refreshData="getUserData" @soalSelesai="handleSoalSelesai"/>
 
             </div>
-                <SoalEssayRmib v-if="showEssay"/>
+                <SoalEssayRmib v-if="showEssay" :customerRmibId="customerRmibId" @essaySelesai="handleEssaySelesai"/>
         </section>
         <section class="bg-white py-[40px]">
-            <ReservasiFooter/>
         </section>
     </Layout>
 </template>
@@ -58,8 +58,8 @@
 import { computed, onBeforeMount, onMounted, ref } from 'vue'
 import Layout from '@/Layout/Customer/Layout.vue';
 import Instruksi from './intruksi.vue';
-// import SelesaiTest from '@/components/REMAKE/HasilTest/SelesaiTest/SelesaiTest.vue';
-import SoalTest from './SoalRmib.vue';
+import SelesaiTest from '@/components/REMAKE/HasilTest/SelesaiTest/SelesaiTest.vue';
+import SoalTest from './soalrmib1.vue';
 import SoalEssayRmib from './SoalEssayRmib.vue';
 import { useStore } from 'vuex';
 import initAPI from '@/api/api';
@@ -73,13 +73,14 @@ import modalCekProfile from '@/components/modalCekProfile/modalCekProfile.vue';
 const isKebijakanPrivasi = ref(true)
 const dataProfileInclomplete = cekDataProfile()
 
-// const subMessage = `Kerja yang bagus! Kamu telah menyelesaikan Tes <span class="font-bold">Intelligent Quotient (IQ)</span>. Mari lihat hasilnya dan temukan lebih banyak tentang potensi diri Kamu!`
+const subMessage = `Kerja yang bagus! Kamu telah menyelesaikan Tes <span class="font-bold">Rothwell Miller Interest Blank (RMIB)</span>. Mari lihat hasilnya dan temukan lebih banyak tentang potensi diri Kamu!`
 
 const store = useStore()
 const loading = ref(true)
-// const isTested = ref(false)
+const isTested = ref(false)
 const isInstruksi = computed(() => store.getters.getStatusIsInstruksi)
 const customerId = ref(null)
+const customerRmibId = ref(null)
 const customerGen = ref('')
 const showEssay = ref(false); // State untuk menampilkan soal essay
 
@@ -91,9 +92,14 @@ const getUserData = async() => {
 
         const userData = await initAPI('post', 'login', formData, token)
         console.log(`data user`, userData.data)
-        customerId.value = userData.data.customer.id
-        // isTested.value = userData.data.customer.customers_rmib == null ? false : true
+        customerId.value = userData.data.customer.id  
         customerGen.value = userData.data.customer.gender == 'Perempuan' ? 2 : 1
+        
+        if (userData.data.customer.customers_rmib) {
+            customerRmibId.value = userData.data.customer.customers_rmib.id || ''; 
+        } else {
+            customerRmibId.value = null; // Explicitly set to null if not available
+        }        // isTested.value = userData.data.customer.customers_rmib == null ? false : true
     } catch (error) {
         console.log(`cek`, error)
         Swal.fire({
@@ -108,16 +114,16 @@ const getUserData = async() => {
     }
 }
 
-// onMounted(()=>{
-//     getUserData()
-// })
-
 const toggleKebijakanPrivasi = () => {
     isKebijakanPrivasi.value = !isKebijakanPrivasi.value
 }
 
 const handleSoalSelesai = () => {
     showEssay.value = true;
+};
+
+const handleEssaySelesai = () => {
+    isTested.value = true;
 };
 
 onMounted(async() => {
@@ -133,8 +139,11 @@ onBeforeMount(() => {
     if(localStorage.getItem('isKebijakanPrivasi') == 'Ya'){
         isKebijakanPrivasi.value = false
     }
-    if(localStorage.getItem('selesaibagianpilihan') == 'done'){
+    if(localStorage.getItem('soalrmib1') == 'selesai'){
         showEssay.value = true
+    }
+    if (localStorage.getItem('soalrmib2') === 'selesai') {
+        isTested.value = true;
     }
 })
 </script>
